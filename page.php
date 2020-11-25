012c6baed95677c3c235a8812a0c84f17da3c757
Micah Gentzel <mgentzel@allebach.com>
2020-11-25 13:09:44 -0500

    site_main class to page main

--- page.php
+++ page.php
@@ -5,7 +5,7 @@ get_header();
 
 <div id="primary">
 
-    <main id="main">
+    <main id="main" class="site_main">
 
         <?php while(have_posts()): the_post(); ?>
             <?php get_template_part('template-parts/content', 'page'); ?>
